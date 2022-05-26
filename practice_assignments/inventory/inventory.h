c738d70e7cc1ae31de7f7b2746ea07bf29015ad6
ocularDisparity <39426265+ocularDisparity@users.noreply.github.com>
2022-05-26 13:02:00 -0500

    Update inventory.h

--- practice_assignments/inventory/inventory.h
+++ practice_assignments/inventory/inventory.h
@@ -1,6 +1,22 @@
 #ifndef INVENTORY.H
 #define INVENTORY.H
+#include <vector>
+#include <string>
 
+class item {
+ private:
+  string name;
+  int itemNo;
+  string description;
+ 
+ public:
+  //setters
   
+  //getters
+  string getname();
+  int getnumber();
+  string getdesc();
+  
+}
 
 #endif
